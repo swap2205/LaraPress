ef20a08070ef86498d74c5aca8c9d204f24708c9
swap2205 <swap2205@gmail.com>
2020-08-17 23:30:08 +0530

    updates

--- public/themes/admin/partials/aside.blade.php
+++ public/themes/admin/partials/aside.blade.php
@@ -5,7 +5,7 @@
     <a href="{{url('admin/dashboard')}}" class="brand-link">
       <img src="@asset("img/AdminLTELogo.png")" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">LaraPress</span>
     </a>
 
     <!-- Sidebar -->
